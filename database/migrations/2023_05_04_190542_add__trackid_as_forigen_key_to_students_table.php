0ff8e6e5b830ad1152e7c65bd3989bd1d4248111
mustafaramadan1 <mustafa.ramadan165@gmail.com>
2023-05-05 17:27:27 +0300

    First Commit

---
+++ database/migrations/2023_05_04_190542_add__trackid_as_forigen_key_to_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+            
+            
+            $table->unsignedBigInteger('track_id');
+            $table->foreign('track_id')->references('id')->on('track')->onDelete('CASCADE');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+            $table->dropForeign('students_track_id_foreign');
+            $table->dropColumn('track_id');
+        });
+    }
+};
